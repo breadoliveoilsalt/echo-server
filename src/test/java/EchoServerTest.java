e540bfe895a904716356c3dccdf168515af26b67
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-20 16:53:00 -0500

    Refactor EchoServer tests

--- src/test/java/EchoServerTest.java
+++ src/test/java/EchoServerTest.java
@@ -1,18 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.junit.Assert.assertSame;
 
 public class EchoServerTest {
 
-    @Test public void testRunBindsTheServerSocketToAPort() throws IOException {
-        int samplePort = 8000;
-        ServerSokket serverSokketSpy = new ServerSokketSpy();
-        ConnectionProtocol sendMessageProtocolSpy = new SendMessageProtocolSpy();
+    int samplePort = 8000;
+    ServerSokket serverSokketSpy;
+    ConnectionProtocol sendMessageProtocolSpy;
 
+    @Before
+    public void init() {
+        this.serverSokketSpy = new ServerSokketSpy();
+        this.sendMessageProtocolSpy = new SendMessageProtocolSpy();
+    }
+
+    @After
+    public void tearDown() {
+        this.serverSokketSpy = null;
+        this.sendMessageProtocolSpy = null;
+    }
+
+    @Test public void testRunBindsTheServerSocketToAPort() throws IOException {
         EchoServer.run(samplePort, serverSokketSpy, sendMessageProtocolSpy);
 
         assertThat((((ServerSokketSpy) serverSokketSpy).methodLog()), hasItems("establishAndListenAtPort()"));
@@ -20,11 +33,9 @@ public class EchoServerTest {
     }
 
     @Test public void testRunTellsServerSocketToReturnAConnectedSocketUponConnection() throws IOException {
-        int samplePort = 8000;
-        ServerSokket serverSokketSpy = new ServerSokketSpy();
-        ConnectionProtocol sendMessageProtocolSpy = new SendMessageProtocolSpy();
-
         EchoServer.run(samplePort, serverSokketSpy, sendMessageProtocolSpy);
         assertThat((((ServerSokketSpy) serverSokketSpy).methodLog()), hasItems("acceptConnectionAndReturnConnectedSocket()"));
     }
+
+
 }
