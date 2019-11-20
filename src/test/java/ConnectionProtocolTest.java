0eb1a84cb057ee099895b5ee1f06a7f8823adbce
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-20 15:35:26 -0500

    Pass second test for Connection Protocol

--- src/test/java/ConnectionProtocolTest.java
+++ src/test/java/ConnectionProtocolTest.java
@@ -8,8 +8,13 @@ public class ConnectionProtocolTest {
 
     @Test public void testHandleConnectionSendsHeyThereToSocketOutputStream() throws IOException {
         Sokket sokketSpy = new SokketSpy();
-
         ConnectionProtocol.handleConnection(sokketSpy);
         assertThat(((SokketSpy) sokketSpy).methodLog(), hasItems("sendToOutputStream()"));
+    }
+
+    @Test public void testHandleConnectionClosesTheSocket() throws IOException {
+        Sokket sokketSpy = new SokketSpy();
+        ConnectionProtocol.handleConnection(sokketSpy);
+        assertThat(((SokketSpy) sokketSpy).methodLog(), hasItems("close()"));
     }
 }
