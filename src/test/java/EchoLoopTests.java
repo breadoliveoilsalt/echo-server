41b143a71727defcaabef9e30dc91db050602f62
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-22 16:29:50 -0500

    Draft asserts for EchoLoop tests

---
+++ src/test/java/EchoLoopTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import java.io.IOException;
+
+public class EchoLoopTests {
+
+    @Test
+    public void testRunBeginsALoopThatReadsLineFromClientAndSendsSameMessageBackToClient() throws IOException {
+    }
+
+    // @Test public void testRunContinuesTheLoopUntilClientSendsExitMessage() throws IOException {
+    // }
+
+
+}
