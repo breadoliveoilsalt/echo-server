c601cb95ddf0fa3b93797462cf85ad599cf81625
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-26 10:37:39 -0500

    In EchoLoop, connection closes when client types exit with exclamation point

--- src/main/java/EchoLoop.java
+++ src/main/java/EchoLoop.java
@@ -4,7 +4,8 @@ public class EchoLoop implements ConnectionProtocol {
 
     public void handleConnection(Sokket sokket) throws IOException {
         String clientMessage;
-        while ((clientMessage = sokket.readLine()) != "exit!") {
+        while (!(clientMessage = sokket.readLine()).equals("exit!")) {
+            System.out.println(clientMessage);
             sokket.sendToOutputStream(clientMessage);
         }
         sokket.close();
