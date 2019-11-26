cf5e2632b726fcede6608d52b556e09fd13dc21e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-26 10:02:54 -0500

    Delete prompts to user as part of my testing

--- src/main/java/EchoLoop.java
+++ src/main/java/EchoLoop.java
@@ -3,10 +3,9 @@ import java.io.IOException;
 public class EchoLoop implements ConnectionProtocol {
 
     public void handleConnection(Sokket sokket) throws IOException {
-        sokket.sendToOutputStream("Enter some text!");
         String clientMessage;
         while ((clientMessage = sokket.readLine()) != "exit!") {
-            sokket.sendToOutputStream("** Back attcha: " + clientMessage + " **");
+            sokket.sendToOutputStream(clientMessage);
         }
         sokket.close();
     }
