04dee8e82e3cba6b72026c667f7691d640f50ff2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-26 10:16:15 -0500

    Refactor EchoLoop tests

--- src/test/java/EchoLoopTests.java
+++ src/test/java/EchoLoopTests.java
@@ -10,33 +10,38 @@ import java.util.List;
 public class EchoLoopTests {
 
     private EchoLoop loop;
+    private MockSokket sokket;
+    private List messagesFromClient;
+    private List expectedSentMessages;
 
     @Before
     public void init() {
         loop = new EchoLoop();
+        sokket = new MockSokket();
+        messagesFromClient = new ArrayList();
+        expectedSentMessages = new ArrayList();
     }
 
     @Test
     public void testRunBeginsALoopThatReadsLineFromClientAndSendsSameMessageBackToClient() throws IOException {
-        MockSokket sokket = new MockSokket();
-        List messageFromClient = new ArrayList<String>(Arrays.asList("Hello!", "exit!"));
-        sokket.setMockMessagesToReceiveFromClient(messageFromClient);
-        List expectedMessageFromClient = new ArrayList(Arrays.asList("Hello!"));
+        messagesFromClient.addAll(Arrays.asList("Hello!", "exit!"));
+        sokket.setMockMessagesToReceiveFromClient(messagesFromClient);
+        expectedSentMessages.add("Hello!");
 
         assertTrue(sokket.getMessagesSentToClient().isEmpty());
         loop.handleConnection(sokket);
 
-        assertEquals(expectedMessageFromClient, sokket.getMessagesSentToClient());
+        assertEquals(expectedSentMessages, sokket.getMessagesSentToClient());
     }
 
      @Test public void testRunContinuesTheLoopUntilClientSendsExitMessage() throws IOException {
-        MockSokket sokket = new MockSokket();
-        List messagesFromClient = new ArrayList(Arrays.asList("Hello!", "How are you?", "Bye!", "exit!"));
+        messagesFromClient.addAll(Arrays.asList("Hello!", "How are you?", "Bye!", "exit!"));
         sokket.setMockMessagesToReceiveFromClient(messagesFromClient);
-        List expectedMessagesSentToClient = new ArrayList(Arrays.asList("Hello!", "How are you?", "Bye!"));
+        expectedSentMessages.addAll(Arrays.asList("Hello!", "How are you?", "Bye!"));
+
         loop.handleConnection(sokket);
 
-        assertEquals(expectedMessagesSentToClient, sokket.getMessagesSentToClient());
+        assertEquals(expectedSentMessages, sokket.getMessagesSentToClient());
      }
 
 
