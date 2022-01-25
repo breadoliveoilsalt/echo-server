a0fb8e1a9d754aca7a1bf73243ad1da8ef25214d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2022-01-25 16:23:48 -0500

    Add check for checkstyle

--- runCheckstyle.sh
+++ runCheckstyle.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+set -Eeou pipefail
+
+function checkForCheckstyle {
+  command -v checkstyle >/dev/null 2>&1
+}
+
+function printNoCheckstyleAndExit {
+  echo "You don't have checkstyle installed. You can get it with \`brew install checkstyle\`" && exit 1
+
+}
+
+checkForCheckstyle || printNoCheckstyleAndExit
+
 echo "Running Git Pre-Commit Hook"
